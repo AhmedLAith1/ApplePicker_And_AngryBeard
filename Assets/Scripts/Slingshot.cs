8435478ee188cea595adb448e5ecdd74c837f19e
AhmedLAith1 <111230728+AhmedLAith1@users.noreply.github.com>
2025-03-12 16:57:25 +0300

    Initial commit

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+    public float velocityMult = 8f;
+
+    [Header("Set Dynamically")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+    private Rigidbody projectileRigidbody;
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    }
+
+    void OnMouseEnter()
+    {
+        launchPoint.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(prefabProjectile, launchPos, Quaternion.identity);
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();
+        projectileRigidbody.isKinematic = true;
+    }
+
+    void Update()
+    {
+        if (!aimingMode) return;
+
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        projectile.transform.position = launchPos + mouseDelta;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.linearVelocity = -mouseDelta * velocityMult;  // ✅ التصحيح هنا
+            projectile = null;
+        }
+    }
+}
